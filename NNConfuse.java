25fe277816e844866f2a37ef270e476ec6c281f5
enigmadan <enigmadan@gmail.com>
2013-06-17 02:47:35 +0900

    Update NNConfuse.java

--- NNConfuse.java
+++ NNConfuse.java
@@ -1,5 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -8,30 +8,37 @@ import java.util.ArrayList;
  *
  */
 public class NNConfuse {
-  public NNConfuse(ArrayList<DataObject> train,ArrayList<DataObject> test) throws FileNotFoundException{
+	public NNConfuse(ArrayList<DataObject> train,ArrayList<DataObject> test) throws IOException{
+		//initialize ArrayLists
 		ArrayList<String> classes = new ArrayList<String>();
 		ArrayList<PosNeg> confusion = new ArrayList<PosNeg>();
+		//what are all the classes?
 		for(int i = 0;i<train.size();i++){
 			if(!classes.contains(train.get(i).classAtt)){
 				classes.add(train.get(i).classAtt);
 			}
 		}
+		//even more possible classes
 		for(int i = 0;i<test.size();i++){
-
 			if(!classes.contains(test.get(i).classAtt)){
 				classes.add(test.get(i).classAtt);
 			}
-
 		}
+		
+		//fill arraylist with classes
 		for(int i = 0;i<classes.size();i++){
 			confusion.add(new PosNeg(classes.get(i)));
 		}
+		
+		//fill array incorrect guess list with classes
 		for(int i = 0;i<confusion.size();i++){
 			for(int j = 0;j<confusion.size();j++){
 				confusion.get(i).ar.add(new PosNeg(confusion.get(j).className));
 			}
 		}
-
+		
+		//check if class actually matches prediction and put
+		//in correct position in confusion matrix
 		for(int i=0;i<test.size();i++){
 			for(int j=0;j<confusion.size();j++){
 				if(confusion.get(j).className.equals(test.get(i).classAtt)){
@@ -47,25 +54,29 @@ public class NNConfuse {
 				}
 			}
 		}
-
-		PrintWriter write = new PrintWriter("myResult");
-		write.print(" ,");
+		
+		//print first row classes
+		FileWriter writer = new FileWriter("myResult");
+		writer.write(" ,");
 		for(int i=0;i<confusion.size();i++){
-			write.print(confusion.get(i).className+",");
+			writer.write(confusion.get(i).className+",");
 		}
+		
+		//print everything else in its place.
 		for(int i=0;i<confusion.size();i++){
-			write.print("\n"+confusion.get(i).className+",");
+			writer.write("\n"+confusion.get(i).className+",");
 			for(int j = 0;j<confusion.size();j++){
 				if(i == j){
-					write.print(confusion.get(i).number+",");
+					writer.write(confusion.get(i).number+",");
 				}else{
-					write.print(confusion.get(i).ar.get(j).number+",");
+					writer.write(confusion.get(i).ar.get(j).number+",");
 				}
 			}
 		}
-		write.close();
+		writer.close();
 	}
-	public class PosNeg{
+	//private class posneg contains a class with its number of occurences and an arraylist of misses
+	private class PosNeg{
 		ArrayList<PosNeg> ar = new ArrayList<PosNeg>();
 		String className;
 		int number = 0;
