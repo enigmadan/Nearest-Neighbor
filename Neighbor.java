ee056a4ff384cc89de712d61b9c92b4de57acb0d
JSeptienR <jseptienr@hotmail.com>
2013-06-16 18:22:28 -0600

    Update Neighbor.java

--- Neighbor.java
+++ Neighbor.java
@@ -1,10 +1,13 @@
-public class Neighbor {
-  double dist;
+public class Neighbor implements Comparable<Neighbor>{
+	double dist;
 	DataObject neighbor;
-	
+
 	public Neighbor(double d, DataObject o){
-		dist=d;
-		neighbor=o;
+		dist= d;
+		neighbor= o;
 	}
 	
+	public int compareTo(Neighbor n){
+		return this.dist > n.dist ? 1: this.dist < n.dist ? -1: 0;
+	}
 }
